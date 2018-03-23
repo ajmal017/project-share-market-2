58e7d82196057b9f0a6646040e2c108896df5127
Stephen M <s3408386@student.rmit.edu.au>
2018-03-24 08:23:16 +1100

    Working on Stock market to chart interactions

--- market-share-app/routes/web.php
+++ market-share-app/routes/web.php
@@ -4,18 +4,9 @@
 routes are loaded by the RouteServiceProvider within a group which
 contains the "web" middleware group. Now create something great!*/
 
-
-
-
-
-
-
-Route::get('/', function () {
-  return view('welcome');
-});
-
-Route::get('/stockmarkettest', function () {
-  return view('stockmarkettest');
+Route::get(
+    '/', function () {
+        return view('welcome');
 });
 
 Route::get(
