cfcd004922b293d6801134e8f87b9f5183411c88
Stephen M <s3408386@student.rmit.edu.au>
2018-03-24 08:46:12 +1100

    CSS and grid additions
    Added css container padding for main content elements.
    Added a CSS grid onto the /listing page to container 1 or more charts

--- market-share-app/resources/views/pages/listing.blade.php
+++ market-share-app/resources/views/pages/listing.blade.php
@@ -5,8 +5,21 @@
     <script type = "text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
 
     <!-- PAGE SPECIFIC CONTENT GOES HERE -->
-    <div>
-        <canvas id="myChart" width="400" height="400"></canvas>
+
+    <div class="main-container">
+        <div class="grid-container">
+            <div class="grid-item">
+                <canvas id="myChart" width="25%" height="25%"></canvas>
+            </div>
+            <div class="grid-item">2</div>
+            <div class="grid-item">3</div>
+            <div class="grid-item">4</div>
+            <div class="grid-item">5</div>
+            <div class="grid-item">6</div>
+            <div class="grid-item">7</div>
+            <div class="grid-item">8</div>
+            <div class="grid-item">9</div>
+        </div> 
     </div>
     <!-- END OF CONTENT -->
 
