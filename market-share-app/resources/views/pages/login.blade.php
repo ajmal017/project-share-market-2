f5274681262912aa8f42ca62907522692d3eeb70
jrkell <37535302+jrkell@users.noreply.github.com>
2018-03-24 12:51:43 +1000

    Updated pw-strength JS

--- market-share-app/resources/views/pages/login.blade.php
+++ market-share-app/resources/views/pages/login.blade.php
@@ -4,6 +4,15 @@
 
     <!-- PAGE SPECIFIC CONTENT GOES HERE -->
 
+    <h1>Create an account</h1>
+    <form>
+        <p><input type="text" name="username"></p>
+        <p>
+            <input type="password" name="password" id="password"><br>
+        </p>
+        <p><input type="submit" value="Log In"></p>
+    </form>
+
     <!-- END OF CONTENT -->
 
 @endsection
