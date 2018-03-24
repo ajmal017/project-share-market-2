b40237cf1e921cc528d4f5b2d461fd11d8ef0dbe
shannajcr <37362104+shannajcr@users.noreply.github.com>
2018-03-25 00:10:46 +1100

    footer edit

--- market-share-app/resources/views/layouts/main-template.blade.php
+++ market-share-app/resources/views/layouts/main-template.blade.php
@@ -11,17 +11,20 @@
 			crossorigin="anonymous"></script>
         <title>Stock Your Socks Off</title>
     </head>
+    <header>
+        <div id="logo">
+            <a href="landing"><img id="sysoimg" src="/images/SYSOlogo2.png" align="left"/></a>
+        </div>
+        <div id="nav">
+            <p>testing</p>
+        </div>
+    </header>
     <body>
-        <header>
-            <div>
-                <a href='landing'><img id="sysoimg" src="/images/SYSOlogo2.png" align="left"/></a>
-            </div>
-            <div id="nav">
-                <p>testing</p>
-            </div>
-        </header>
         @yield('content')
     </body>
     <footer>
+            <div>
+                <p>2018 Wolf Pack Of Wall Street Limited ABN 12 345 678</p>
+            </div>            
     </footer>
 </html>
