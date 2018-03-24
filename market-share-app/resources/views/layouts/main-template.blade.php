16f2b769eaffd2a045aaff8c022b37a1810c23b3
shannajcr <37362104+shannajcr@users.noreply.github.com>
2018-03-24 15:05:10 +1100

    initial dev commit

--- market-share-app/resources/views/layouts/main-template.blade.php
+++ market-share-app/resources/views/layouts/main-template.blade.php
@@ -8,6 +8,13 @@
 
     </head>
     <body>
+        <header>
+            <div class="logo">
+                <a href='pages/landing'><img id="sysoimg" src="../images/SYSOlogo2.png" align="left"/></a>
+            </div>
+            <nav>
+            </nav>
+        </header>
         @yield('content')
     </body>
     <footer>
