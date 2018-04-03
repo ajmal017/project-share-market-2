36bd876a8fe8ae01162f9fd366d6aa0dfdc57376
shannajcr <37362104+shannajcr@users.noreply.github.com>
2018-04-03 13:40:11 +1000

    sign styling edit

--- market-share-app/resources/views/pages/signin.blade.php
+++ market-share-app/resources/views/pages/signin.blade.php
@@ -31,7 +31,7 @@
                     <div>
                         <label for="password"><!-- {{ __('Password') }} --></label>
                         <div>
-                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Enter Password" name="password" required>
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Enter password" name="password" required>
 
                             @if ($errors->has('password'))
                                 <span class="invalid-feedback">
--- market-share-app/resources/views/pages/signup.blade.php
+++ market-share-app/resources/views/pages/signup.blade.php
@@ -5,56 +5,58 @@
     <!-- PAGE SPECIFIC CONTENT GOES HERE -->
     <div class = "sysoBox sysoBoxFlex">
         <div class = "sysoContent sysoContent100">
-            <h1>Create an account</h1>
-            <form method="POST" action="{{ route('register') }}">
-                @csrf
-                <div>
-                    <label for="name">{{ __('Name') }}</label>
+            <h1 id = "rego">Stock Your Socks Off Registration</h1>
+            <div id = "signup">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
                     <div>
-                        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-                        @if ($errors->has('name'))
-                            <span class="invalid-feedback">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                        @endif
+                        <label for="name">{{ __('Name') }}</label>
+                        <div>
+                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" placeholder="Enter name" value="{{ old('name') }}" required autofocus>
+                            @if ($errors->has('name'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
+                            @endif
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label for="email">{{ __('E-Mail Address') }}</label>
                     <div>
-                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
+                        <label for="email">{{ __('E-Mail Address') }}</label>
+                        <div>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" placeholder="Enter email address" value="{{ old('email') }}" required>
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label for="password">{{ __('Password') }}</label>
                     <div>
-                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
+                        <label for="password">{{ __('Password') }}</label>
+                        <div>
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Enter password" required>
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label for="password-confirm">{{ __('Confirm Password') }}</label>
                     <div>
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                        <label for="password-confirm">{{ __('Confirm Password') }}</label>
+                        <div>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Re-enter password" required>
+                        </div>
                     </div>
-                </div>
-                <div>
                     <div>
-                        <button type="submit">
-                            {{ __('Register') }}
-                        </button>
+                        <div>
+                            <button type="submit">
+                                {{ __('Register') }}
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
     <!-- END OF CONTENT -->
