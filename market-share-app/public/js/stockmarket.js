3c9ef7eec9d7363172e85bcff18327235003fcbd
jrkell <37535302+jrkell@users.noreply.github.com>
2018-04-04 15:28:30 +1000

    Updated Listing Page

--- market-share-app/public/js/stockmarket.js
+++ market-share-app/public/js/stockmarket.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
                 success: function (results) {
                     asx_results = results;
                     if (results != '') {
-                        $("#company_details").html("<b>" + results[0]['company_name'] + "</b><br>" + results[0]['gics_industry']);
-                        $("#get_daily_data").html('<input type="button" value="Get Daily Data" id="query_daily_data">');
+                        $("#company_details").html("<b><a href='/listing/" + results[0]['company_code'] + "'>" + results[0]['company_name'] + "</a></b><br>" + results[0]['gics_industry']);
+                        //$("#get_daily_data").html('<input type="button" value="Get Daily Data" id="query_daily_data">');
                     } else{
                         $("#company_details").html("<b>No companies found matching that ASX code</b>");
                     }
@@ -48,8 +48,8 @@ $(document).ready(function () {
             success: function (results) {
                 if (results != '') {
                     $('#asx_code').val(results[0]['company_code']);
-                    $("#company_details").html("<b>" + results[0]['company_name'] + "</b><br>" + results[0]['gics_industry']);
-                    $("#get_daily_data").html('<input type="button" value="Get Daily Data" id="query_daily_data">');
+                    $("#company_details").html("<b><a href='/listing/" + results[0]['company_code'] + "'>" + results[0]['company_name'] + "</a></b><br>" + results[0]['gics_industry']);
+                    //$("#get_daily_data").html('<input type="button" value="Get Daily Data" id="query_daily_data">');
                 } else {
                     $("#company_details").html("<b>No companies found matching that ASX code</b>");
                 }
--- market-share-app/resources/views/pages/listing.blade.php
+++ market-share-app/resources/views/pages/listing.blade.php
@@ -7,40 +7,55 @@
 
     <!-- PAGE SPECIFIC CONTENT GOES HERE -->
    
+    <!-- Extra scripts specific to this page -->
+    <script type="text/javascript">
+        function addPurchaseForm() {
+            $form = "<input type='text' placeholder='Enter Qty'></input>";
+            $confirm = "<button type='button'>Confirm Purchase</button>";
+            $total = "<p>Total Price: $XXXX </p>";
+        
+            GEBI("buyForm").innerHTML = $form + $total + $confirm ;
+        }
+    </script>
+    
 
     
         
     <div id = "temporaryBox">
-    <div id = "asx_code">{{$symbol}}</div>
-        <div class="grid-item" id="company_details"></div>
-
-    </div>
-    <script type="text/javascript">
-        function getCompanyName(symbol) {
-            var mysym = symbol;
-            var asx_results;
         
-            
-            $.ajax({
-                
-                url: "/listing/companycode/" + symbol,
-                success: function (results) {
-                    asx_results = results;
+        <div class="grid-item" id="company_details"><b>{{$data[0]->company_name}}</b><br>{{$data[0]->gics_industry}}</div>
         
-                    if (results != '') {
-                        $("#company_details").html("<b>" + results[0]['company_name'] + "</b><br>" + results[0]['gics_industry']);
-                    } else{
-                        $("#company_details").html("<b>No companies found matching that ASX code</b>");
-                    }
-                }
-            });
-        }
-
-        getCompanyName(document.getElementById("asx_code").textContent);
         
-
-    </script>
+        <table>
+            <tr>
+                <th>Company Name</th>
+                <td>{{$data[0]->company_name}}</td>
+            <tr>
+            <tr>
+                <th>Industry</th>
+                <td>{{$data[0]->gics_industry}}</td>
+            <tr>
+            <tr>
+                <th>ASX Company Code</th>
+                <td>{{$data[0]->company_code}}</td>
+            <tr>
+            <tr>
+                <th>Current Stock Price</th>
+                <td>...</td>
+            <tr>
+        </table>
+        <form>
+            <button type='button' onclick="addPurchaseForm()">Buy Shares</button>
+            <div id="buyForm" class="grid-item"></div>
+        </form>
     
+
+    
+    </div>
+    
+  
+    
+
     <!-- END OF CONTENT -->
 
 @endsection
