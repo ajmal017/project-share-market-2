0a82a3dd82da2264fe1345de2ec4818bbd51629d
shannajcr <37362104+shannajcr@users.noreply.github.com>
2018-04-06 09:15:33 +1000

    Back page link

--- market-share-app/resources/views/pages/about.blade.php
+++ market-share-app/resources/views/pages/about.blade.php
@@ -1,8 +1,16 @@
 @extends('layouts/main-template')
 
+@section('link')
+<!-- ADD LINKS DISPLAYED ON HEADER NAV BAR -->
+    <a href="#" onClick="history.go(-1);return true;">Back</a>
+    <a href='landing'>Home</a>
+@endsection
+
 @section('content')
 
     <!-- PAGE SPECIFIC CONTENT GOES HERE -->
+
+    <a href="#" onClick="history.go(-1);return true;">Back</a>
 
     <!-- END OF CONTENT -->
 
