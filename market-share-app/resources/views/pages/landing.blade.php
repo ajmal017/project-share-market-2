b8d7e58ed44fdc5869c49c9009ff506391fc371f
Joel Graves <s3353237@student.rmit.edu.au>
2018-04-07 10:37:06 +0930

    Tuned welcome page styles
    Tuned a few welcome page styles. Fixed disappearing image in portrait mode. Added hamburger menu link style.

--- market-share-app/resources/views/pages/landing.blade.php
+++ market-share-app/resources/views/pages/landing.blade.php
@@ -2,7 +2,7 @@
 
 @section('link')
 <!-- ADD LINKS DISPLAYED ON HEADER NAV BAR -->
-    <a href='about'>About/FAQ</a>
+    <a class = "sysoLink" href='about'>About/FAQ</a>
 @endsection
 
 @section('content')
@@ -10,10 +10,11 @@
     <!-- PAGE SPECIFIC CONTENT GOES HERE -->
     <div class = "sysoBox sysoBoxFlex">
         <div id = "welcomeBox" class = "sysoContent sysoContent50">
+            <div id = "sysoWelcomeImage"></div>
         </div>
         <div id = "landing" class = "sysoContent sysoContent50">
-            <a class = "wpLink" href="/signup">Sign Up</a>
-            <a class = "wpLink" href="/signin">Login</a>
+            <a class = "sysoLink" href="/signup">Sign Up</a>
+            <a class = "sysoLink" href="/signin">Login</a>
             <div id = "intro">
                 <p>Buy and sell shares to make your way up the leaderboard.</p>
                 <p>Sign up now and be given $1,000,000 in game currency!</p>
