50bf5433ef7de128a1fdc76b0210d6618e087da4
Stephen M <s3408386@student.rmit.edu.au>
2018-04-11 18:04:18 +1000

    Fixed issue between deploying between local and prod server

--- market-share-app/config/database.php
+++ market-share-app/config/database.php
@@ -11,7 +11,7 @@ return [
     | to use as your default connection for all database work. Of course
     | you may use many connections at once using the Database library.
     |
-    */
+ */
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
@@ -29,7 +29,7 @@ return [
     | so make sure you have the driver for your particular database of
     | choice installed on your machine before you begin development.
     |
-    */
+     */
 
     'connections' => [
 
@@ -46,13 +46,13 @@ return [
             // 'database' => env('DB_DATABASE', 'forge'),
             // 'username' => env('DB_USERNAME', 'forge'),
             // 'password' => env('DB_PASSWORD', ''),
-            'host'      => $_SERVER['RDS_HOSTNAME'],
-            'port'      => $_SERVER['RDS_PORT'],
-            'database'  => $_SERVER['RDS_DB_NAME'],
-            'username'  => $_SERVER['RDS_USERNAME'],
-            'password'  => $_SERVER['RDS_PASSWORD'],
+            'host' => (isset($_SERVER['RDS_HOSTNAME']) == true ? $_SERVER['RDS_HOSTNAME'] : env('DB_HOST', '127.0.0.1')),
+            'port' => (isset($_SERVER['RDS_PORT']) == true ? $_SERVER['RDS_PORT'] : env('DB_PORT', '3306')),
+            'database' => (isset($_SERVER['RDS_DB_NAME']) == true ? $_SERVER['RDS_DB_NAME'] : env('DB_DATABASE', 'forge')),
+            'username' => (isset($_SERVER['RDS_USERNAME']) == true ? $_SERVER['RDS_USERNAME'] : env('DB_USERNAME', 'forge')),
+            'password' => (isset($_SERVER['RDS_PASSWORD']) == true ? $_SERVER['RDS_PASSWORD'] : env('DB_USERNAME', 'forge')),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset'   => 'utf8',
+            'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'strict' => true,
@@ -94,7 +94,7 @@ return [
     | your application. Using this information, we can determine which of
     | the migrations on disk haven't actually been run in the database.
     |
-    */
+     */
 
     'migrations' => 'migrations',
 
@@ -107,7 +107,7 @@ return [
     | provides a richer set of commands than a typical key-value systems
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
-    */
+     */
 
     'redis' => [
 
