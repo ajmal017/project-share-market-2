a67c6cd168ed648b1b89541dcb16f2ad86f31306
shannajcr <37362104+shannajcr@users.noreply.github.com>
2018-04-16 13:44:13 +1000

    leaderboard data

--- market-share-app/resources/views/pages/community.blade.php
+++ market-share-app/resources/views/pages/community.blade.php
@@ -4,6 +4,36 @@
 
     <!-- PAGE SPECIFIC CONTENT GOES HERE -->
 
+                <div class="friends">
+                    <h1>Top 10</h1>
+                    <table class="friendList">
+                    <tr id = "tableHeader">
+                        <th>Name</th>
+                        <th>Total Worth</th>
+                        <th>Friend</th>
+                    </tr>
+                     
+                     <?php 
+                            use App\User;
+                            // query userid in open transactions table
+                            $json = DB::table('users')->get();
+                            $data = json_decode($json);
+                            $name=null;
+                            $balance=0.00;
+                            foreach ($data as $line) {
+                                $name=($line->name);
+                                $balance=($line->account_balance);
+                                echo "<tr>";
+                                echo "<td>".$name."</td>";
+                                echo "<td>".$balance."</td>";
+                                echo "<td><input type='checkbox' name='friend' {{ old('friend') ? 'checked' : '' }}></td>";
+                                echo "</tr>";
+                            }
+                        ?>
+
+                    </table>
+                </div>
+
     <!-- END OF CONTENT -->
 
 @endsection
