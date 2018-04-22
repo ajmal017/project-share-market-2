2812ad1f84ca0a5eaee190eb15b8812f3c28faa5
Joel Graves <s3353237@student.rmit.edu.au>
2018-04-22 11:52:26 +0930

    Added dynamic links to landing page
    Now shows a link to account if already logged in.

--- market-share-app/resources/views/pages/landing.blade.php
+++ market-share-app/resources/views/pages/landing.blade.php
@@ -27,13 +27,20 @@
             <div id = "sysoWelcomeImage"></div>
         </div>
         <div id = "landing" class = "sysoContent sysoContent50">
-            <a class = "sysoLink" href="/signup">Sign Up</a>
-            <a class = "sysoLink" href="/signin">Login</a>
-            <div id = "intro">
-                <p>Buy and sell shares to make your way up the leaderboard.</p>
-                <p>Sign up now and be given $1,000,000 in game currency!</p>
-                <a href='about'>About/FAQ</a>
-            </div>
+            @if(Auth::check())
+                <div id = "intro">
+                    <p>You're already logged in! Click the link below to continue buying and selling shares.</p>
+                    <a class = "sysoLink" href="/account">Show me my account</a>
+                </div>
+            @else
+                <a class = "sysoLink" href="/signup">Sign Up</a>
+                <a class = "sysoLink" href="/signin">Login</a>
+                <div id = "intro">
+                    <p>Buy and sell shares to make your way up the leaderboard.</p>
+                    <p>Sign up now and be given $1,000,000 in game currency!</p>
+                    <a class = "sysoLink" href='about'>About/FAQ</a>
+                </div>
+            @endif
         </div>
     </div>
     <!-- END OF CONTENT -->
