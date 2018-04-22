8552b20baea24970b200754dcf69f1b6772be47c
jrkell <37535302+jrkell@users.noreply.github.com>
2018-04-22 13:42:01 +1000

    added confirmation to sell button

--- market-share-app/resources/views/pages/listing.blade.php
+++ market-share-app/resources/views/pages/listing.blade.php
@@ -79,6 +79,7 @@
                 <input type='hidden' name='code' value='{{$data[0]->company_code}}'>
                 <div id="buyForm" class="grid-item"></div>
             </form>
+            <a href="#" onClick="history.go(-1);return true;">Back</a>
         </div>
 
         <div class = "sysoContent sysoContent50">
