8c82eba1f9d58b0f5a0303ba6b40cb2a2ac9fc48
shannajcr <37362104+shannajcr@users.noreply.github.com>
2018-04-26 22:02:37 +1000

    community page checking duplicates

--- market-share-app/app/Http/Controllers/FriendController.php
+++ market-share-app/app/Http/Controllers/FriendController.php
@@ -13,15 +13,18 @@ class FriendController extends Controller
   
   public function insertFriend($fid){
         $user_id=Auth::user()->id;
-        $data = array('user_id'=>$user_id,'friend_id'=>$fid);
-        DB::table('friends')->insert($data);
+        $data=array('user_id'=>$user_id,'friend_id'=>$fid);
+        $check=DB::table('friends')->where('user_id', '=', $user_id)->where('friend_id', '=', $fid);
+        if ($check->count() == 0) {
+            DB::table('friends')->insert($data);
+        }
+        else{
+            echo "alert Friend already exists";
+        }
   }
 
   public function deleteFriend($fid){
-        echo "Testing Unfriend";
         $user_id=Auth::user()->id;
-       // $data = array('user_id'=>$user_id,'friend_id'=>$fid);
-       //DB::table('friends')->delete($data);
         DB::table('friends')->where('user_id', '=', $user_id)->where('friend_id', '=', $fid)->delete();
   }
 
